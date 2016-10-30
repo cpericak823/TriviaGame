d2bf92f248f4e8d4b497de7a2d3c6a4e001d3a41
Cassie <cassie.pericak@northwestern.edu>
2016-10-30 17:25:17 -0500

    added pseudo code

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//set timer for 120 seconds 
+//append paragraph elements to display all questions and true or false options using jquery
+//capture click option radio button
+//when timer runs out, compare selected options to true answers
+//append dom to show number of questions answered and number of right answers and restart button
+//capture click of restart button
+//reset game
