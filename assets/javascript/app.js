060733c157e0618acf4bc851dc5e0a9b01363be3
Cassie <cassie.pericak@northwestern.edu>
2016-10-31 18:24:53 -0500

    added radio buttons for true and false

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,29 @@
 //Start the game
+var counter = 120;
+
+function twoMinutes() {
+    $('#timer').html('Time Remaining:' + counter);
+    counter--;
+}
 $(document).ready(function() {
+
     //set timer for 120 seconds 
-    setTimeout(twoMinutes, 1000 * 120);
+    setInterval(twoMinutes, 1000);
     console.log('timer started');
 
-    //append the timer ID to display timer using jquery
-    function twoMinutes() {
-        $('#timer').html('Time Remaining:' + setTimeout());
-    }
 
     //append paragraph elements to display all questions using jquery
     function displayQuestions() {
-        $('#question1').html('Does the US border four other countries?'); //false
-        $('#question2').html('Is a rose the national flower of the US?'); //true
-        $('#question3').html('Is the Missouri river the longest river in the US?'); //true
-        $('#question4').html('Is the US the fourth largest country in the world?'); //true
-        $('#question5').html('Did FDR serve 5 terms?'); //false
-        $('#question6').html('Was there only 46 states when the Cubs won the World Series last?'); //true
-        $('#question7').html('There were three presidents who signed the Declaration of Independance went on to become president.'); //false
-        $('#question8').html('Is English the official language of the US?'); //false
-        $('#question9').html('Are there are 5 timezones in the US?'); //false
-        $('#question10').html('Is Juneau is the largest city in the US?'); //true
+        $('#question1').html('Does the US border four other countries? <br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //false
+        $('#question2').html('Is a rose the national flower of the US? <br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //true
+        $('#question3').html('Is the Missouri river the longest river in the US? <br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //true
+        $('#question4').html('Is the US the fourth largest country in the world?<br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //true
+        $('#question5').html('Did FDR serve 5 terms?<br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //false
+        $('#question6').html('Was there only 46 states when the Cubs won the World Series last?<br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //true
+        $('#question7').html('There were three presidents who signed the Declaration of Independance went on to become president.<br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //false
+        $('#question8').html('Is English the official language of the US?<br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //false
+        $('#question9').html('Are there are 5 timezones in the US?<br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //false
+        $('#question10').html('Is Juneau is the largest city in the US?<br> <label> <input type="radio"/> True </label><br> <label> <input type="radio"/> False </label>'); //true
     }
     //call displayQuestions function
     displayQuestions();
