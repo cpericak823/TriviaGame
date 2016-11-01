87be4af55c2e97b9cf1d69affa88b33a73e15812
Cassie <cassie.pericak@northwestern.edu>
2016-11-01 13:07:02 -0500

    updated pseudo code and tried adjusting clearing interval timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -32,29 +32,28 @@ $(document).ready(function() {
     displayQuestions();
     console.log('questions displayed');
 
-
-    //use setTimeout when timer runs out, 
-    clearInterval(twoMinutes, 1000 * 120, finishGame());
-    console.log('timer ended');
-
-    console.log('answers displayed');
-    //update the DOM with the answers to the questions
-    function finishGame() {
-        $('#question1').html('Does the US border four other countries? <br><label>False</label>');
-        $('#question2').html('Is a rose the national flower of the US? <br><label>True</label>');
-        $('#question3').html('Is the Missouri river the longest river in the US? <br><label>True</label>');
-        $('#question4').html('Is the US the fourth largest country in the world?<br><label>True</label>');
-        $('#question5').html('Did FDR serve 5 terms?<br><label>False</label>');
-        $('#question6').html('Was there only 46 states when the Cubs won the World Series last?<br><label>True</label>');
-        $('#question7').html('There were three presidents who signed the Declaration of Independance went on to become president.<br><label>False</label>');
-        $('#question8').html('Is English the official language of the US?<br><label>False</label>');
-        $('#question9').html('Are there are 5 timezones in the US?<br><label>False</label>');
-        $('#question10').html('Is Juneau is the largest city in the US?<br><label>True</label>');
-
-    }
-
-    //compare selected options to true answers
-    //append dom to show number of questions answered and number of right answers and restart button
-    //capture click of restart button
-    //reset game
+    //capture on click event
 });
+//use setTimeout when timer runs out, 
+clearInterval(timer, 1000 * 120);
+console.log('timer ended');
+
+//compare selected options to true answers and display number right/wrong on the screen
+//update the DOM with the answers to the questions
+function finishGame() {
+    $('#question1').html('Does the US border four other countries? <br><label>False</label>');
+    $('#question2').html('Is a rose the national flower of the US? <br><label>True</label>');
+    $('#question3').html('Is the Missouri river the longest river in the US? <br><label>True</label>');
+    $('#question4').html('Is the US the fourth largest country in the world?<br><label>True</label>');
+    $('#question5').html('Did FDR serve 5 terms?<br><label>False</label>');
+    $('#question6').html('Was there only 46 states when the Cubs won the World Series last?<br><label>True</label>');
+    $('#question7').html('There were three presidents who signed the Declaration of Independance went on to become president.<br><label>False</label>');
+    $('#question8').html('Is English the official language of the US?<br><label>False</label>');
+    $('#question9').html('Are there are 5 timezones in the US?<br><label>False</label>');
+    $('#question10').html('Is Juneau is the largest city in the US?<br><label>True</label>');
+
+}
+//call the finish game function when timer runs out
+
+finishGame();
+console.log('answers displayed');
